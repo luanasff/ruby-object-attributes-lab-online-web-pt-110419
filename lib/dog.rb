c3858ac2e90bbc5df64d224ed90a4d7f45e054c3
Luana Faria Lane <luanasfff@gmail.com>
2019-11-20 21:06:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  def name 
+    @name = "Fido" 
+  end 
+  def name=(dogs_name)
+    @name=dogs_name
+  end
+  
+  def breed 
+    @breed = "Beagle"
+   end 
+  def breed= (dogs_breed)
+    @breed = dogs_breed
+  end   
+end 
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person 
+   
+  def name 
+    @name = "Beyonce" 
+  end 
+  def name=(persons_name)
+    @name=persons_name
+  end
+  
+  def job 
+    @job = "Singer"
+  end 
+  def job= (persons_job)
+    @job = persons_job
+  end
+end 
